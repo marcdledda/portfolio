0d3719c127477a03fa792219358260611969e0e3
marcdledda <marcdl636@gmail.com>
2018-05-05 02:26:01 -0500

    fix routing so pages do not stack; reworked navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import Main from './components/Main';
 import './components/navigation.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import TypeOffMain from './components/TypeOffMain';
+import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="container navBar">
-          <div className="row">
-            <div className="col-sm-12 text-center navSmall"><h1>Marc Ledda</h1></div>
-            <div className="col-sm text-center navSmall">Work</div>
-            <div className="col-sm text-center navSmall">Resume</div>
-            <div className="col-sm text-center navSmall">About</div>
+      <Router>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-6 navLink"><Link to="/"><h1>Marc Ledda</h1></Link></div>
+              <div className="col-sm align-self-center navLink">Work</div>
+              <div className="col-sm align-self-center navLink">Resume</div>
+              <div className="col-sm align-self-center navLink">About</div>
+            </div>
           </div>
+          <Route exact path="/" component={Main} />
+          <Route path="/TypeOff" component={TypeOffMain} />
         </div>
-        <Main />
-      </div>
+      </Router>
     );
   }
 }
--- src/components/TypeOff.js
+++ src/components/TypeOff.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
 import {
-    BrowserRouter as Router,
-    Route,
+    // BrowserRouter as Router,
+    // Route,
     Link
   } from 'react-router-dom';
-import TypeOffMain from './TypeOffMain';
 import typeOffBanner from '../media/typeOffIMG.png';
 import './typeOff.css'
 
 class TypeOff extends Component {
     render(){
         return (
-            <Router>
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <img className="img-fluid typeOffImg" src={typeOffBanner} alt="TypeOff game banner"/>
-                        </div>
-                        <div className="col-12 align-self-center"><h2 className="typeOffTitle">TypeOff</h2></div>
-                        <div className="col-12 typeOffDesc"><h4>A web game that uses typing as its main mechanic.</h4></div>
-                        <div className="col-12 readBtn">
-                            <Link to="/casestudy/TypeOff" className="btn btn-secondary"><h6>Read More</h6></Link>
-                        </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <img className="img-fluid typeOffImg" src={typeOffBanner} alt="TypeOff game banner"/>
                     </div>
-                    <Route path="/casestudy/TypeOff" component={TypeOffMain}/>
+                    <div className="col-12 align-self-center"><h2 className="typeOffTitle">TypeOff</h2></div>
+                    <div className="col-12 typeOffDesc"><h4>A web game that uses typing as its main mechanic.</h4></div>
+                    <div className="col-12 readBtn">
+                        <Link to="/TypeOff" className="btn btn-secondary"><h6>Read More</h6></Link>
+                    </div>
                 </div>
-            </Router>
+            </div>
         )
     }
 }
