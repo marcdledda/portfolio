a625625f09ad3ffe6c42bc1d5287af5a880e1b39
marcdledda <marcdl636@gmail.com>
2018-05-12 13:13:26 -0500

    create footer test

--- src/components/TypeOff.js
+++ src/components/TypeOff.js
@@ -5,22 +5,26 @@ import {
     Link
   } from 'react-router-dom';
 import typeOffBanner from '../media/typeOffIMG.png';
+import Footer from './Footer';
 import './typeOff.css'
 
 class TypeOff extends Component {
     render(){
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <img className="img-fluid typeOffImg" src={typeOffBanner} alt="TypeOff game banner"/>
-                    </div>
-                    <div className="col-12 align-self-center"><h2 className="typeOffTitle">TypeOff</h2></div>
-                    <div className="col-12 typeOffDesc"><h4>A web game that uses typing as its main mechanic.</h4></div>
-                    <div className="col-12 readBtn">
-                        <Link to="/TypeOff" className="btn btn-secondary"><h6 className="readMore">Read More</h6></Link>
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <img className="img-fluid typeOffImg" src={typeOffBanner} alt="TypeOff game banner"/>
+                        </div>
+                        <div className="col-12 align-self-center"><h2 className="typeOffTitle">TypeOff</h2></div>
+                        <div className="col-12 typeOffDesc"><h4>A web game that uses typing as its main mechanic.</h4></div>
+                        <div className="col-12 readBtn">
+                            <Link to="/TypeOff" className="btn btn-secondary"><h6 className="readMore">Read More</h6></Link>
+                        </div>
                     </div>
                 </div>
+                <Footer />
             </div>
         )
     }
