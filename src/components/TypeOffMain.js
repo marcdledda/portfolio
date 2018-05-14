a3219cf8794a5b1de009f0a01371abf5dffa461b
marcdledda <marcdl636@gmail.com>
2018-05-14 11:22:11 -0500

    finish final design section

--- src/components/TypeOffMain.js
+++ src/components/TypeOffMain.js
@@ -3,6 +3,11 @@ import typeOffEX from '../media/typeOff/typeOffEX.png';
 import typeOffPersona from '../media/typeOff/typeOffPersona.png';
 import sketches from '../media/typeOff/sketches.png';
 import sketchSpecific from '../media/typeOff/sketchSpecific.png';
+import mainMenu from '../media/typeOff/mainMenu.png';
+import tutorialGame from '../media/typeOff/tutorialGame.png';
+import myScores from '../media/typeOff/myScores.png';
+import leaderboards from '../media/typeOff/leaderboards.png';
+import following from '../media/typeOff/following.png';
 import Footer from './Footer';
 import './typeOffMain.css';
 
@@ -67,7 +72,7 @@ class TypeOffMain extends Component {
                             <p>
                                 Parker plays video games often, as they are his main source of entertainment. Typically, he plays around 1-3 hours a day, if time permits. He is attracted towards the competitive aspect of a game, constantly looking to improve his score.
                             </p>
-                            <p className="personaQuote">
+                            <p className="personaQuote font-italic">
                                 "I want to play a fun game that also allows me to improve my skill."
                             </p>
                         </div>
@@ -80,10 +85,47 @@ class TypeOffMain extends Component {
                             <p className="px-lg-5 mb-lg-5">
                                 I decided to place all interactions within the HTML canvas, rather than having a menu interface outside the canvas. Initially, I had thought of making the canvas vertical to accomodate mobile users, but decided against it because of the target audience's preferred device being a computer.
                             </p>
-                            <img className="img-fluid" src={sketchSpecific} alt="Specific sketches of typeoff"/>
-                            <p className="px-lg-5 mb-lg-5">
+                            <img className="img-fluid mb-lg-3" src={sketchSpecific} alt="Specific sketches of typeoff"/>
+                            <p className="px-lg-5">
                                 Parker's score is stored in Google Firebase and then displayed in both the "Scores" page and the "Leaderboards"pages, allowing Parker to keep track of their progress. The "Friends List" page is a way for Parker to compare his score with other users.
                             </p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-12 text-center px-lg-5 py-lg-4 mx-auto mt-5">
+                            <h2 className="mb-lg-4">Final Design</h2>
+                            <img className="img-fluid" src={mainMenu} alt="Main Menu"/>
+                            <p className="font-italic pb-lg-5 mb-lg-5">
+                                Main Menu
+                            </p>
+                            <img className="img-fluid" src={tutorialGame} alt="Main Menu"/>
+                            <p className="font-italic mb-0">
+                                Tutorial into the Main Game
+                            </p>
+                            <p className="font-italic pb-lg-5 mb-lg-5">
+                                A tutorial page was added so that Parker is informed on how to play the game, as well as load the API in the background.
+                            </p>
+                            <div className="row">
+                                <div className="col-4">
+                                    <img className="img-fluid" src={myScores} alt="My Scores Page"/>
+                                    <p className="font-italic">
+                                        User Scores Page
+                                    </p>
+                                </div>
+                                <div className="col-4">
+                                    <img className="img-fluid" src={leaderboards} alt="Leaderboards Page"/>
+                                    <p className="font-italic">
+                                        Leaderboards
+                                    </p>
+                                </div>
+                                <div className="col-4">
+                                    <img className="img-fluid" src={following} alt="Following Page"/>
+                                    <p className="font-italic">
+                                        Following/Followers
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
